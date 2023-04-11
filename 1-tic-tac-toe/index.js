8ee92735af5a480ade36af104bcd4c8001c90fcd
Alisa K <53273292+ectupand@users.noreply.github.com>
2023-04-11 04:49:51 -0700

    Add files via upload

---
+++ 1-tic-tac-toe/index.js
@@ -0,0 +1,111 @@
+const CROSS = 'X';
+const ZERO = 'O';
+const EMPTY = ' ';
+
+const container = document.getElementById('fieldWrapper');
+
+game()
+
+function game(){
+    startGame();
+    addResetListener();
+
+}
+
+function startGame () {
+    renderGrid(3);
+}
+
+function renderGrid (dimension) {
+    container.innerHTML = '';
+
+    for (let i = 0; i < dimension; i++) {
+        const row = document.createElement('tr');
+        for (let j = 0; j < dimension; j++) {
+            const cell = document.createElement('td');
+            cell.textContent = EMPTY;
+            cell.addEventListener('click', () => cellClickHandler(i, j));
+            row.appendChild(cell);
+        }
+        container.appendChild(row);
+    }
+}
+
+function cellClickHandler (row, col) {
+    if (findCell(row, col).textContent === EMPTY){
+        console.log(`Clicked on cell: ${row}, ${col}`);
+        renderSymbolInCell(CROSS, row, col);
+
+    }
+}
+
+class Point {
+    constructor(row, col) {
+        this.row = row;
+        this.col = col;
+    }
+}
+
+class Grid {
+    constructor(){
+        this.grid = [
+        ['', '', ''],
+        ['', '', ''],
+        ['', '', '']
+        ]
+    }
+}
+
+
+function renderSymbolInCell (symbol, row, col, color = '#333') {
+    const targetCell = findCell(row, col);
+
+    targetCell.textContent = symbol;
+    targetCell.style.color = color;
+}
+
+function findCell (row, col) {
+    const targetRow = container.querySelectorAll('tr')[row];
+    return targetRow.querySelectorAll('td')[col];
+}
+
+function addResetListener () {
+    const resetButton = document.getElementById('reset');
+    resetButton.addEventListener('click', resetClickHandler);
+}
+
+function resetClickHandler () {
+    renderGrid(3);
+    console.clear();
+}
+
+
+/* Test Function */
+/* Победа первого игрока */
+function testWin () {
+    clickOnCell(0, 2);
+    clickOnCell(0, 0);
+    clickOnCell(2, 0);
+    clickOnCell(1, 1);
+    clickOnCell(2, 2);
+    clickOnCell(1, 2);
+    clickOnCell(2, 1);
+}
+
+/* Ничья */
+function testDraw () {
+    clickOnCell(2, 0);
+    clickOnCell(1, 0);
+    clickOnCell(1, 1);
+    clickOnCell(0, 0);
+    clickOnCell(1, 2);
+    clickOnCell(1, 2);
+    clickOnCell(0, 2);
+    clickOnCell(0, 1);
+    clickOnCell(2, 1);
+    clickOnCell(2, 2);
+}
+
+function clickOnCell (row, col) {
+    findCell(row, col).click();
+}
